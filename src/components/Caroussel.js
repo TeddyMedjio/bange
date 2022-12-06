406d650723b4e1ad54806e7e142071129413e214
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2022-12-06 16:46:47 +0100

    update json

--- src/components/Caroussel.js
+++ src/components/Caroussel.js
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Caroussel.css";
-import Fade from "react-reveal/Fade";
 
 export default class SimpleSlider extends Component {
   constructor(props) {
@@ -32,34 +31,28 @@ export default class SimpleSlider extends Component {
         <Slider {...settings} className="slider overflow-hidden">
           <div className="bg-hero-pattern1 bg-no-repeat bg-cover h-[400px] md:h-[635px]">
             <div className="container mx-auto w-[85%] pt-24 md:pt-40">
-              <Fade left>
-                <h1 className=" text-[#ffffff] md:text-white font-bold text-[45px] text-center md:text-left leading-[1] md:text-6xl lg:text-7xl md:w-1/2 ">
-                  Des solutions de financements à court terme
-                </h1>
-              </Fade>
-              <Fade bottom>
-                <p className="hidden lg:block text-white md:text-lg mt-10 md:w-2/5">
-                  Grâce à leurs durées inférieure à un an. Nos solutions de
-                  financement vont vous permettre de financer vos activités
-                  quotidiennes ...
-                </p>
-              </Fade>
+              <h1 className=" text-[#ffffff] md:text-white font-bold text-[45px] text-center md:text-left leading-[1] md:text-6xl lg:text-7xl md:w-1/2 ">
+                Des solutions de financements à court terme
+              </h1>
+
+              <p className="hidden lg:block text-white md:text-lg mt-10 md:w-2/5">
+                Grâce à leurs durées inférieure à un an. Nos solutions de
+                financement vont vous permettre de financer vos activités
+                quotidiennes ...
+              </p>
             </div>
           </div>
 
           <div className="bg-hero-pattern2 bg-no-repeat bg-cover h-[400px] md:h-[635px]">
             <div className="container mx-auto w-[85%] pt-24 md:pt-40">
-              <Fade left>
-                <h1 className=" text-[#ffffff] md:text-white font-bold text-[45px] text-center md:text-left leading-[1] md:text-6xl lg:text-7xl md:w-1/2 ">
-                  Encore plus de possibilité chez nous
-                </h1>
-              </Fade>
-              <Fade bottom>
-                <p className="hidden lg:block text-white md:text-lg mt-10 md:w-2/5">
-                  Dans un monde qui change, nous accompagnons tous nos clients
-                  et nos collaborateurs dans la construction d’un futur durable.
-                </p>
-              </Fade>
+              <h1 className=" text-[#ffffff] md:text-white font-bold text-[45px] text-center md:text-left leading-[1] md:text-6xl lg:text-7xl md:w-1/2 ">
+                Encore plus de possibilité chez nous
+              </h1>
+
+              <p className="hidden lg:block text-white md:text-lg mt-10 md:w-2/5">
+                Dans un monde qui change, nous accompagnons tous nos clients et
+                nos collaborateurs dans la construction d’un futur durable.
+              </p>
             </div>
           </div>
         </Slider>
