a00de583eac988a21b438c7b229dc3c5298e5a58
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2022-12-08 05:22:01 +0100

    goodmap

--- src/components/google.js
+++ src/components/google.js
@@ -1,64 +1,56 @@
 import React from "react";
-import mapboxgl from "mapbox-gl";
 import map from "../img/icons/map.svg";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoidGVkZHkyMzciLCJhIjoiY2xiNm9raHl4MDMxdjN2bGVqcjkxazNvNiJ9.eOKv2XPQi-AG_USX4rMreg";
+import Map, {
+  Marker,
+  NavigationControl,
+  Popup,
+  FullscreenControl,
+  GeolocateControl,
+} from "react-map-gl";
+import { useState } from "react";
+function Mapp() {
+  const [lng, setLng] = useState(9.696536930830325);
+  const [lat, setLat] = useState(4.051807752792534);
+  const [zoom, setZoom] = useState(15);
 
-export default class Mapp extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lng: 9.696536930830325,
-      lat: 4.051807752792534,
-      zoom: 15,
-    };
-    this.mapContainer = React.createRef();
-  }
-
-  componentDidMount() {
-    const { lng, lat, zoom } = this.state;
-    const map = new mapboxgl.Map({
-      container: this.mapContainer.current,
-      style: "mapbox://styles/teddy237/clbc89taf000c14moitsiwd83",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    map.on("move", () => {
-      this.setState({
-        lng: map.getCenter().lng.toFixed(4),
-        lat: map.getCenter().lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2),
-      });
-    });
-  }
-  render() {
-    const { lng, lat, zoom } = this.state;
-    return (
-      <div className="relative">
-        <div className="z-50 absolute container w-[2%]">
-          <div className="bg-[#018539] rounded-b-md w-80 pb-16">
-            <h3 className="text-white font-semibold text-4xl w-3/4 pl-10 pt-10">
-              Notre réseau au Cameroun
-            </h3>
-            <div className="pl-10 mt-5 flex items-start w-3/5">
-              <img src={map} className="w-5 mr-3 pt-1" />
-              <p className="text-white text-sm font-bold uppercase">
-                carte des implatations
-              </p>
-            </div>
+  return (
+    <div className="relative">
+      <div className="z-50 absolute container w-[2%]">
+        <div className="bg-[#018539] rounded-b-md w-80 pb-16">
+          <h3 className="text-white font-semibold text-4xl w-3/4 pl-10 pt-10">
+            Notre réseau au Cameroun
+          </h3>
+          <div className="pl-10 mt-5 flex items-start w-3/5">
+            <img src={map} className="w-5 mr-3 pt-1" />
+            <p className="text-white text-sm font-bold uppercase">
+              carte des implatations
+            </p>
           </div>
         </div>
-        {/* <div className="sidebar">
-          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-        </div> */}
-        <div
-          ref={this.mapContainer}
-          className="map-container"
-          style={{ width: "100%", height: "671px" }}
-        />
       </div>
-    );
-  }
+      <Map
+        mapboxAccessToken="pk.eyJ1IjoidGVkZHkyMzciLCJhIjoiY2xiZWphYWRwMDA0bzQwdXl1eDQwd25obCJ9.MB_Y-EUBwf7lnkB4qTfL-A"
+        style={{
+          width: "100%",
+          height: "671px",
+        }}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: zoom,
+        }}
+        mapStyle="mapbox://styles/teddy237/clbc89taf000c14moitsiwd83"
+      >
+        {/* <Marker longitude={lng} latitude={lat} /> */}
+        <NavigationControl position="bottom-right" />
+        <FullscreenControl />
+
+        <GeolocateControl />
+      </Map>
+    </div>
+  );
 }
+
+export default Mapp;
